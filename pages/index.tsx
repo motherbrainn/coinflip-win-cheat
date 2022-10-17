3e3157730c8041e4fcb7474a38105c455c195433
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-10-17 12:00:11 -0700

    add some styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
-import styles from "../styles/Home.module.css";
 
 enum Winner {
   HEADS = "heads",
@@ -13,28 +10,49 @@ const Home: NextPage = () => {
   const hKeyHold = useRef(false);
   const tKeyHold = useRef(false);
 
+  const coin = useRef<null | HTMLElement>(null);
+
   const [winner, setWinner] = useState<null | string>(null);
 
-  const onCoinflip = () => {
-    if (hKeyHold.current === true && tKeyHold.current === false) {
-      setWinner(Winner.HEADS);
-    }
-    if (tKeyHold.current === true && hKeyHold.current === false) {
-      setWinner(Winner.TAILS);
-    }
-    if (
-      (hKeyHold.current === false && tKeyHold.current === false) ||
-      (hKeyHold.current === true && tKeyHold.current === true)
-    ) {
-      Math.floor(Math.random() * 2)
-        ? setWinner(Winner.HEADS)
-        : setWinner(Winner.TAILS);
-    }
+  const startAnimation = () => {
+    setWinner(null);
+    console.log("animation");
+    coin.current?.classList.remove("coin");
+    coin.current?.classList.add("coin2");
+
+    setTimeout(() => {
+      coin.current?.classList.remove("coin2");
+      coin.current?.classList.add("coin");
+    }, 1000);
+    // test.style.animation = "spin 1s linear infinite";
   };
 
-  useEffect(() => console.log("winner: ", winner), [winner]);
+  const onCoinflip = () => {
+    startAnimation();
+
+    setTimeout(() => {
+      if (hKeyHold.current === true && tKeyHold.current === false) {
+        setWinner(Winner.HEADS);
+      }
+      if (tKeyHold.current === true && hKeyHold.current === false) {
+        setWinner(Winner.TAILS);
+      }
+      if (
+        (hKeyHold.current === false && tKeyHold.current === false) ||
+        (hKeyHold.current === true && tKeyHold.current === true)
+      ) {
+        Math.floor(Math.random() * 2)
+          ? setWinner(Winner.HEADS)
+          : setWinner(Winner.TAILS);
+      }
+    }, 1000);
+  };
 
   useEffect(() => {
+    coin.current = document.getElementById("coin");
+
+    console.log(coin.current);
+
     document.addEventListener("keydown", (event) => {
       if (event.key === "h") {
         hKeyHold.current = true;
@@ -56,8 +74,12 @@ const Home: NextPage = () => {
 
   return (
     <div>
-      <button onClick={onCoinflip}>Flip Coin</button>
-      {winner && <div>{`Winner: ${winner}`}</div>}
+      <div className="browser">
+        <h1>coinflip</h1>
+        <button onClick={onCoinflip}>Flip Coin</button>
+        <div className={"coin flip silver"} id="coin"></div>
+        {winner && <h2>{`Winner: ${winner}`}</h2>}
+      </div>
     </div>
   );
 };
