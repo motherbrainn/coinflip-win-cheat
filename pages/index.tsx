44581cfd99f887ec6ccaef699cfcb9700a2ffd4f
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-10-20 23:19:58 -0700

    add more spam

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from "next";
+import Image from "next";
 import { useEffect, useRef, useState } from "react";
+import Modal from "react-modal";
+import warningIcon from "../assets/bliss.png";
 
 enum Winner {
   HEADS = "heads",
@@ -14,6 +17,11 @@ const Home: NextPage = () => {
   const coin = useRef<null | HTMLElement>(null);
 
   const [winner, setWinner] = useState<null | string>(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   const startAnimation = () => {
     setWinner(null);
@@ -65,20 +73,67 @@ const Home: NextPage = () => {
         tKeyHold.current = false;
       }
     });
+
+    setTimeout(() => {
+      setModalOpen(true);
+    }, 6000);
   }, []);
 
   return (
     <div>
       <div className="browser">
+        <div className="banner">WHATS YOUR IQ?? CLICK TO FIND OUT!</div>
         <h1>coinflip</h1>
         <h2>click coin to flip!</h2>
-        <div
-          className={"coin flip silver"}
-          id="coin"
-          onClick={onCoinflip}
-        ></div>
+        <div className={"coin flip silver"} id="coin" onClick={onCoinflip}>
+          {" "}
+        </div>
         {winner ? <h2>{`Winner: ${winner}`}</h2> : <h2>???</h2>}
       </div>
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+        style={{
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+          },
+        }}
+      >
+        <div className="modal">
+          <img src={warningIcon} />
+          <h1 style={{ color: "red" }}>WARNING!</h1>
+          <h2 style={{ color: "red" }}>YOUR COMPTER MAY BE INFECTED:</h2>
+          <p>
+            `System Detected <strong style={{ color: "red" }}>(2)</strong>{" "}
+            Potentially <ins style={{ color: "green" }}>Malicious</ins> Viruses:
+            <ins style={{ fontStyle: "italic", color: "green" }}> Rootkit</ins>
+            <strong>.Sirefef.Spy</strong> and <strong>Trojan.FakeAV-</strong>{" "}
+            <ins style={{ fontStyle: "italic", color: "green" }}>DOWNLOAD.</ins>{" "}
+            Your Personal &{" "}
+            <ins style={{ fontStyle: "italic", color: "green" }}>
+              Financial Information
+            </ins>
+            <strong> MAY NOT BE SAFE.</strong>
+          </p>
+          <h2 style={{ color: "red" }}>
+            To Remove Viruses, Call Tech Support Online Now:
+          </h2>
+          <h1>1 (866) 527-4049</h1>
+          <p>
+            <ins style={{ fontStyle: "italic", color: "green" }}>
+              High Priority Virus Removal.
+            </ins>{" "}
+            Call Line
+          </p>
+          <button onClick={() => setModalOpen(false)}>Close</button>
+        </div>
+      </Modal>
     </div>
   );
 };
